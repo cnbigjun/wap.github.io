999a7e52cecc28ad54d1366737a492c600957217
bigjuncn <446262388@qq.com>
2023-06-02 23:23:06 -0500

    init

---
+++ Lab5/lab5.js
@@ -0,0 +1,15 @@
+function sum(arr) {
+    return arr.filter(num => num > 20).reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+}
+
+let arr = [21,1,22,23]
+console.log(sum(arr))
+
+const getNewArray = function(arr) {
+    return arr.filter(function(str) {
+        return str.length >= 5 && str.includes('a');
+    });
+}
+
+let strArray=['aaaaa','aa','bbbbb']
+console.log(getNewArray(strArray))
